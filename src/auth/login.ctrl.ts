81115cd6211eb4b285c492f0cfcaf74a3b0a7b77
Boris Ablamunits <borisa@wix.com>
2016-05-08 00:11:25 +0300

    Login fix

--- src/auth/login.ctrl.ts
+++ src/auth/login.ctrl.ts
@@ -2,35 +2,43 @@
 
 class LoginController {
 	isLoggedIn: boolean = true;
+	signInFormVisible: boolean = false;
 	loggedUser: User;
 	email: string;
 	password: string;
 
 	constructor (private $scope: ng.IScope, private $state, private AuthService: AuthServiceProvider) {
-		AuthService.getAuthState().then((response) => {
-			let data: any = response.data;
-			this.isLoggedIn = data.loggedIn;
+		this.updateLoggedUser();
+	}
 
-			if (this.isLoggedIn) {
-				this.loggedUser = {
-					id: data.userId,
-					firstName: data.firstName,
-					lastName: data.lastName,
-					email: data.email,
-					type: UserType.Seeker, // todo
-					photoUrl: data.userPhoto,
-					sex: data.sex
-				};
+	private updateLoggedUser() {
+		this.AuthService.getAuthState().then((response) => {
+			let data: any = response.data;
+
+			if (data.loggedIn === false) {
+				this.isLoggedIn = false;
+				this.loggedUser = null;
+				return;
 			}
 
-			console.log(this.loggedUser);
-		}, (e) => { console.log(e); });
+			this.loggedUser = {
+				id: data.userId,
+				firstName: data.firstName,
+				lastName: data.lastName,
+				email: data.email,
+				type: UserType.Seeker, // todo
+				photoUrl: data.userPhoto,
+				sex: data.sex
+			};
+		});
 	}
 
 	signIn() {
 		this.AuthService.login(this.email, this.password).then((response) => {
 			if (true) { // if success
 				this.isLoggedIn = true;
+				this.updateLoggedUser();
+				this.$state.go('matches');
 			}
 		}, (e) => {
 			console.log(e);
@@ -39,6 +47,8 @@ class LoginController {
 
 	signOut() {
 		this.AuthService.logout();
+		this.updateLoggedUser();
+		this.$state.go('home');
 	}
 };
 
