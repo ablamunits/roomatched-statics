47fb5bdd988027e89f3b5ee9e38745a95d994b5c
Boris Ablamunits <borisa@wix.com>
2016-09-09 23:36:43 +0300

    Random loader phrases :)

--- src/common/loader/loader.drv.ts
+++ src/common/loader/loader.drv.ts
@@ -5,8 +5,12 @@ class LoaderDirectiveController {
 function loaderDirectiveFactory(): ng.IDirective {
 	return <ng.IDirective> {
 		restrict: 'E',
-		require: 'ngModel',
-		template: '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>',
+		template: '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div><div class="loader">{{ randomLoadingMessage }} {{ $scope.randomLoadingMessage }}</div>',
+		link: function (scope) {
+			let randomLoadingMessages = ['Matching you up ...', 'Doing the hard work for you :)', 'You are about to get Roomatched!', 'Looking for great roommates!', 'Your new roommates are gonna be great!'];
+
+			(scope as any).randomLoadingMessage = randomLoadingMessages[Math.floor(Math.random()*randomLoadingMessages.length)];
+		},
 		controller: LoaderDirectiveController,
 		controllerAs: 'ctrl',
 		bindToController: true
